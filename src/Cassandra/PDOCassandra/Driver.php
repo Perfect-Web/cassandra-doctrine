28931d84065760a68b8cf809582545664ce1c906
Alin Jurj <office@perfectweb.ro>
2014-11-11 00:04:59 +0200

    initial commit

--- src/Cassandra/PDOCassandra/Driver.php
+++ src/Cassandra/PDOCassandra/Driver.php
@@ -14,45 +14,7 @@ class Driver extends MySqlDriver implements BaseDriver
 	 */
 	public function connect(array $params, $username = null, $password = null, array $driverOptions = array())
 	{
-
-        $conn = new Connection($this->_constructPdoDsn($params), $username, $password, $driverOptions);
-
-		if (isset($params['dbname'])) {
-			$conn->exec('USE '.$params['dbname']);
-		}
-
-		return $conn;
-
-	}
-
-	/**
-	 * Constructs the MySql PDO DSN.
-	 *
-	 * @param array $params
-	 *
-	 * @return string The DSN.
-	 */
-	private function _constructPdoDsn(array $params)
-	{
-
-		$dsn = 'cassandra:';
-		if (isset($params['host']) && $params['host'] != '') {
-			$dsn .= 'host='.$params['host'].';';
-		}
-
-		foreach ($params['servers'] as $server) {
-			$dsn .= 'host='.$server['host'].';';
-			$dsn .= 'port='.(empty($server['port']) ? 9160 : $server['port']).',';
-		}
-
-		$dsn = trim($dsn, ',');
-
-		if (isset($params['cqlversion'])) {
-			$dsn .= ';cqlversion='.$params['cqlversion'];
-		}
-
-		return $dsn;
-
+        return new \Cassandra\Connection($params['servers'], $params['dbname']);
 	}
 
 	/**
